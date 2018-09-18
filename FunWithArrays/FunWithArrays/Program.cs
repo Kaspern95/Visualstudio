6e0b1b29175c8ba105702925895a24e80e37e9fa
Kaspern95 <34809965+Kaspern95@users.noreply.github.com>
2018-09-18 16:27:02 +0200

    page 111+

---
+++ FunWithArrays/FunWithArrays/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace FunWithArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("** Fun with arrays **");
+
+            //SimpleArrays();
+            //ArrayInitialization();
+            //DeclareImplicitArrays();
+            //ArrayOfObjects();
+
+            Console.ReadLine();
+        }
+        private static void SimpleArrays()
+        {
+            Console.WriteLine(">= Simple array creation.");
+            //creates array of 3 ints 0,1,2
+            int[] myInts = new int[3];
+            myInts[0] = 100;
+            myInts[1] = 200;
+            myInts[2] = 300;
+
+            foreach (int i in myInts)
+            {
+                Console.WriteLine(i);
+            }
+
+            //creates 100 item string array 0-99
+            string[] bookOnDotNet = new string[100];
+            Console.WriteLine();
+
+        }
+        private static void ArrayInitialization()
+        {
+            Console.WriteLine(">= Array Initialization");
+
+            // Array initialization syntax using the "new" keyword.
+            string[] stringArray = new string[]
+            {
+                "one", "two", "three"
+            };
+            Console.WriteLine($"stringArray has {stringArray.Length} elements");
+
+            // Array initialization syntax without using the new keyword.
+            bool[] boolArray = { false, false, true };
+            Console.WriteLine($"boolArray has {boolArray.Length} elements");
+
+            // Array initialization with "new" keyword and size.
+            int[] intArray = new int[4] { 20, 22, 23, 0 };
+            Console.WriteLine($"intArray has {intArray.Length} elements");
+            Console.WriteLine();
+        }
+        private static void DeclareImplicitArrays()
+        {
+            Console.WriteLine(">= Implicit Array initialization");
+
+            //using a as "int[]"
+            var a = new[] { 1,10,100,1000 };
+            Console.WriteLine($"a is a: {a.ToString()}");
+
+            //variables selfdetect
+            var b = new[] {1,1.5,2,2.5 };
+            Console.WriteLine($"b is a: {b.ToString()}");
+
+            //new[] must be used for this method
+            var c = new[] { "hello", null, "world"};
+            Console.WriteLine($"c is a: {c.ToString()}");
+            Console.WriteLine();
+        }
+        private static void ArrayOfObjects()
+        {
+            Console.WriteLine(">= Array of Objects");
+
+            //An array of objects can contain anything
+            object[] myObjects = new object[4];
+            myObjects[0] = 10;
+            myObjects[1] = false;
+            myObjects[2] = new DateTime(1969, 3, 24);
+            myObjects[3] = "Form & void";
+            foreach(object obj in myObjects)
+            {
+                Console.WriteLine($"Type: {obj.GetType()}, value: {obj}");
+            }
+            Console.WriteLine( );
+        }
+
+    }
+}
---
+++ FunWithMethods/FunWithMethods/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace FunWithMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("** fun with methods **");
+            /*
+            int x = 9, y = 10;
+            Console.WriteLine($"before call: X: {x}, Y: {y}");
+            Console.WriteLine($"Answer is: {Add(x,y)}");
+            Console.WriteLine($"after call: X: {x}, Y: {y}");
+            Add(90, 90, out int ans);
+            Console.WriteLine($"90+90 = {ans}");
+            Console.WriteLine();
+
+            int i; string str; bool b;
+            FillTheseValues(out i, out str, out b);
+            Console.WriteLine($"int is {i}");
+            Console.WriteLine($"str is {str}");
+            Console.WriteLine($"bool is {b}");
+            
+            string str1 = "flip";
+            string str2 = "flop";
+            Console.WriteLine($"before:{str1},{str2}");
+            
+
+            SwapStrings(ref str1, ref str2);
+            Console.WriteLine($"after:{str1},{str2}");
+            */
+
+            Console.ReadLine();
+
+        }
+        static int Add(int x, int y)
+        {
+            int ans = x + y;
+            x = 10000;
+            y = 88888;
+            return ans;
+        }
+        static void Add(int x, int y, out int ans)
+        {
+            ans = x + y;
+        }
+        static void FillTheseValues(out int a, out string b, out bool c)
+        {
+            a = 9;
+            b = "string";
+            c = true;
+        }
+        public static void SwapStrings(ref string s1, ref string s2)
+        {
+            string tempStr = s1;
+            s1 = s2;
+            s2 = tempStr;
+        }
+        public static string SimpleReturn(string[] strArray, int position)
+        {
+            return strArray[position];
+        }
+    }
+}

b16894f347f1c9302b27dbbd5cc222df22d182a5
Kaspern95 <34809965+Kaspern95@users.noreply.github.com>
2018-09-26 08:09:21 +0200

    1

--- LoopOpgaver/LoopOpgaver/Program.cs
+++ LoopOpgaver/LoopOpgaver/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LoopOpgaver
 {
@@ -17,10 +13,141 @@ namespace LoopOpgaver
 
             Console.WriteLine("6.1");
 
-            DoWhileLoop6();
+            //DoWhileLoop6();
 
-            Console.WriteLine("hjkasd");
+            Console.WriteLine("6.2");
+
+            //ForLoop6();
+
+            Console.WriteLine("6.3");
+
+            //Opg6_3();
+
+            Console.WriteLine("6.4");
+
+            //opg6_4();
+
+            Console.WriteLine("6.5");
+
+            Opg6_5();
+
+
+            Console.WriteLine("dansgame");
             Console.ReadLine();
+        }
+
+        private static void Opg6_5()
+        {
+            int antal = 0;
+            do
+            {
+
+                int terning1 = rnd.Next(1, 7);
+                int terning2 = rnd.Next(1, 7);
+                int terning3 = rnd.Next(1, 7);
+                int terning4 = rnd.Next(1, 7);
+                int terning5 = rnd.Next(1, 7);
+                int terning6 = rnd.Next(1, 7);
+                string slag = $"{terning1}{terning2}{terning3}{terning4}{terning5}{terning6}";
+
+                if (terning1 == terning2 && terning2 == terning3 && terning3 == terning4 && terning4 == terning5 && terning5 == terning6)
+                {
+                    Console.WriteLine(slag);
+                    Console.WriteLine(antal);   
+                    break;
+
+                }
+                else
+                {
+                    antal++;
+                }
+                
+                
+            } while (true);
+
+
+
+        }
+
+        private static void opg6_4()
+        {
+            string spg;
+            bool check = true;
+            do
+            {
+                Console.WriteLine("indtast film");
+                string film = Console.ReadLine();
+                Console.WriteLine("indtast instruktor");
+                string instruktor = Console.ReadLine();
+                Console.WriteLine("indtast genrer");
+                string genrer = Console.ReadLine();
+                Console.WriteLine("Afslut program (ja/nej:)");
+                spg = Console.ReadLine();
+
+                bool rigtig = true;
+                do
+                {
+                    Console.WriteLine("indtast stjerner");
+                    int stjerner = int.Parse(Console.ReadLine());
+                    if (stjerner < 11 || stjerner >= 0)
+                    {
+                        rigtig = false;
+                    }
+
+                } while (rigtig);
+                if (spg == "ja" || spg == "Ja" || spg == "JA" || spg == "jA")
+                {
+
+                    check = false;
+                }
+                else
+                {
+
+                }
+
+            } while (check);
+        }
+
+        private static void Opg6_3()
+        {
+            string spg;
+            bool check = true;
+            do
+            {
+                Console.WriteLine("indtast film");
+                string film = Console.ReadLine();
+                Console.WriteLine("indtast instruktor");
+                string instruktor = Console.ReadLine();
+                Console.WriteLine("indtast genrer");
+                string genrer = Console.ReadLine();
+                Console.WriteLine("indtast stjerner");
+                int stjerner = int.Parse(Console.ReadLine());
+                Console.WriteLine("Afslut program (ja/nej:)");
+                spg = Console.ReadLine();
+                if (spg == "ja" || spg == "Ja" || spg == "JA" || spg == "jA")
+                {
+                    check = false;
+                }
+                else
+                {
+
+                }
+
+            } while (check);
+        }
+
+        private static void ForLoop6()
+        {
+            int total = 0;
+            for (int i = 0; i < 500; i++)
+            {
+                int number = rnd.Next(1, 7);
+                total += number;
+
+            }
+            double gennemsnit = (double)total / 500;
+            Console.WriteLine("Det gennemsnitlige nummer: {0}", gennemsnit);
+
         }
 
         private static void DoWhileLoop6()
@@ -28,14 +155,15 @@ namespace LoopOpgaver
             int[] intArray = new int[100];
             int cap = 0;
             bool erDetSandt = true;
-            
+            int i = 0;
             do
             {
                 int number = rnd.Next(1001);
+                cap++;
                 if ((number % 10) == 0 && cap < intArray.Length)
                 {
                     Console.WriteLine(number);
-                    cap++;
+
                 }
                 else if (cap >= intArray.Length)
                 {
@@ -43,10 +171,13 @@ namespace LoopOpgaver
                 }
                 else
                 {
-                    cap++;
+
+                    Console.WriteLine(i++);
                 }
+
             } while (erDetSandt);
-                
+            Console.WriteLine(cap - i);
+
         }
 
         private static void WhileLoop6()
---
+++ String manipulation/String manipulation/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stenSang = "Mit navn er Steen, didde lidde lidde lum " +
+                "Med det ekstra ben, didde lidde lidde lum.. " +
+                "Hvorhen jeg går i regn og slud, der peger folk mig altid ud.";
+
+            Console.WriteLine("opgave 7.1");
+            Console.WriteLine("Skriv et ord i sangen \"mit navn er steen\"");
+            string check = Console.ReadLine();
+            bool strcheck = stenSang.Contains(check);
+
+            if (strcheck == true)
+            {
+                Console.WriteLine($"{check} findes i strengen");
+            }
+            else
+            {
+                Console.WriteLine("{0} findes desværre ikke i sangen",0);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("opgave 7.2");
+            string dyr = "ko, gris, hest, flodhest, mus, rotte, hund, kat";
+            string[] destination = dyr.Split(", ".ToCharArray());
+            
+            foreach (string navne in destination)
+            {
+                Console.WriteLine(navne);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Opgave 7.3");
+
+            //string stenSangCheck = Console.ReadLine();
+            int count = 0;
+            int i = 0;
+            while((i = stenSang.IndexOf(check, i)) != -1)
+            {
+                i += check.Length;
+                count++;
+            }
+            Console.WriteLine($"{check} opstod {count} gange");
+            Console.WriteLine();
+
+            Console.WriteLine("Opgave 7.4");
+
+            string reverse = Console.ReadLine();
+            string reversedString = "";
+            for (int i2 = reverse.Length - 1; i2 >= 0; i2--)
+            {
+                reversedString += reverse[i2];
+            }
+            Console.WriteLine(reversedString);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}

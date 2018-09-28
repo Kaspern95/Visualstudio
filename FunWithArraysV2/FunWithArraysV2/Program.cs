ff0ede12c9a133531c72ee7b3a1347e4112c0735
Kaspern95 <34809965+Kaspern95@users.noreply.github.com>
2018-09-29 00:26:10 +0200

    k

---
+++ FunWithArraysV2/FunWithArraysV2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithArraysV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            array[1] = 41;
+
+            int[,] mit2DArray = new int[5, 3];
+            mit2DArray[0, 0] = 51;
+
+            int[,,] mit3DArray = new int[5, 5, 5];
+            mit3DArray[0, 0, 0] = 41;
+
+            int[][] mitJaggedArray = new int[3][];
+            mitJaggedArray[0] = new int[3];
+            mitJaggedArray[1] = new int[5];
+            mitJaggedArray[2] = new int[6];
+            mitJaggedArray[1][2] = 5;
+            mitJaggedArray[2][3] = 5;
+
+        }
+    }
+}
--- String manipulation/String manipulation/Program.cs
+++ String manipulation/String manipulation/Program.cs
@@ -8,62 +8,217 @@ namespace String_manipulation
 {
     class Program
     {
+       
+
         static void Main(string[] args)
         {
+            
             string stenSang = "Mit navn er Steen, didde lidde lidde lum " +
-                "Med det ekstra ben, didde lidde lidde lum.. " +
+                "Med det ekstra ben, didde lidde lidde lum .. " +
                 "Hvorhen jeg går i regn og slud, der peger folk mig altid ud.";
+            /*
+                        Console.WriteLine("opgave 7.1");
+                        Console.WriteLine("Skriv et ord i sangen \"mit navn er steen\"");
+                        string check = Console.ReadLine();
+                        bool strcheck = stenSang.Contains(check);
 
-            Console.WriteLine("opgave 7.1");
-            Console.WriteLine("Skriv et ord i sangen \"mit navn er steen\"");
-            string check = Console.ReadLine();
-            bool strcheck = stenSang.Contains(check);
+                        if (strcheck == true)
+                        {
+                            Console.WriteLine($"{check} findes i strengen");
+                        }
+                        else
+                        {
+                            Console.WriteLine("{0} findes desværre ikke i sangen",0);
+                        }
+                        Console.WriteLine();
 
-            if (strcheck == true)
+                        Console.WriteLine("opgave 7.2");
+                        string dyr = "ko gris hest flodhest mus rotte hund kat";
+                        string[] destination = dyr.Split(' ');
+
+                        foreach (string navne in destination)
+                        {
+                            Console.WriteLine(navne);
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("Opgave 7.3");
+
+                        //string stenSangCheck = Console.ReadLine();
+                        int count = 0;
+                        int i = 0;
+                        while((i = stenSang.IndexOf(check, i)) != -1)
+                        {
+                            i += check.Length;
+                            count++;
+                        }
+                        Console.WriteLine($"{check} opstod {count} gange");
+                        Console.WriteLine();
+
+                        Console.WriteLine("Opgave 7.4");
+
+                        string reverse = Console.ReadLine();
+                        string reversedString = "";
+                        for (int i2 = reverse.Length - 1; i2 >= 0; i2--)
+                        {
+                            reversedString += reverse[i2];
+                        }
+                        Console.WriteLine(reversedString);
+                        */
+
+            //Opgave7_3(stenSang);
+            //Opgave7_3Alt(stenSang);
+            //Opgave7_4();
+            //Opgave7_5();
+            //Opgave7_6();
+            //Opgave7_7();
+            Opgave7_8();
+            Console.ReadLine();
+        }
+
+        private static void Opgave7_8()
+        {
+            string tal1 = "200 kr.";
+            string tal2 = "50 kr.";
+            string tal3 = "5 kr.";
+
+            string res1 = AddMoney(tal1, tal2);
+            string res2 = AddMoney(res1, tal3);
+        }
+
+        private static string AddMoney(string tal1, string tal2)
+        {
+            
+            string amount1 = tal1.Substring(0, tal1.Length - 4);
+            int tal1Kon = int.Parse(amount1);
+
+            string amount2 = tal2.Substring(0, tal2.Length - 4);
+            int tal2Kon = int.Parse(amount2);
+
+            int res = tal1Kon + tal2Kon;
+            string resString = res + " kr.";
+            return resString;
+        }
+
+        private static void Opgave7_7()
+        {
+            string tekst = "Det er ritig let med stringmetoder, at rette ritig mange ens stavefejl.";
+            Console.WriteLine(tekst);
+            string input = Console.ReadLine();
+            string result = tekst.Replace("ritig", input);
+            Console.WriteLine(result);
+        }
+
+        private static void Opgave7_6()
+        {
+            Console.WriteLine("Indtast et tal eller bogstav");
+            string stringNumber = Console.ReadLine();
+            //int number;
+            //bool result = int.TryParse(stringNumber, out number);
+            //Console.WriteLine($"din indtastning er {result} datatypen på dit input er {number.GetType()}");
+            
+            bool erDetEtTal = talTester(stringNumber);
+
+        }
+
+        private static bool talTester(string testString)
+        {
+            bool result = true;
+            char[] array = testString.ToCharArray();
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine($"{check} findes i strengen");
+                if (!(array[i] >= 48 && array[i] <= 57))
+                {
+                    result = false;
+                    break;
+                }
+                
+
+            }
+            return result;
+        }
+
+        private static void Opgave7_5()
+        {
+            string password = "KlauS";
+            Console.WriteLine("Indtast password");
+            string input = Console.ReadLine();
+            string bigInput = input.ToUpper();
+            string bigPassword = password.ToUpper();
+
+            if (bigInput == bigPassword)
+            {
+                Console.WriteLine("success");
+              
             }
             else
             {
-                Console.WriteLine("{0} findes desværre ikke i sangen",0);
+                Console.WriteLine("fail");
+                
             }
-            Console.WriteLine();
+        }
 
-            Console.WriteLine("opgave 7.2");
-            string dyr = "ko, gris, hest, flodhest, mus, rotte, hund, kat";
-            string[] destination = dyr.Split(", ".ToCharArray());
+        private static void Opgave7_4()
+        {
+            string input = Console.ReadLine();
+            char[] array = input.ToCharArray();
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.Write(array[i]);
+
+            }
+        }
+
+        private static void Opgave7_3Alt(string sang)
+        {
+            string[] array;
+            array = sang.Split(',', ' ');
+            string input = Console.ReadLine();
+
+            if (array.Contains(input))
+            {
+                int count = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] == input)
+                    {
+                        count++;
+
+                    }
+                }
+                Console.WriteLine(count);
+
+            }
+            else
+            {
+                Console.WriteLine("Nope");
+            }
+
             
-            foreach (string navne in destination)
-            {
-                Console.WriteLine(navne);
-            }
-            Console.WriteLine();
-            Console.WriteLine("Opgave 7.3");
 
-            //string stenSangCheck = Console.ReadLine();
+        }
+
+        private static void Opgave7_3(string sang)
+        {
+            string check = Console.ReadLine();
             int count = 0;
-            int i = 0;
-            while((i = stenSang.IndexOf(check, i)) != -1)
+            int index = 0;
+
+            while (true)
             {
-                i += check.Length;
-                count++;
+                index = sang.IndexOf(check, index );
+                if (index != -1)
+                {
+                    count++;
+                    index++;
+                }
+                else
+                {
+                    break;
+                }
+                
             }
-            Console.WriteLine($"{check} opstod {count} gange");
-            Console.WriteLine();
+            Console.WriteLine(count);
 
-            Console.WriteLine("Opgave 7.4");
-
-            string reverse = Console.ReadLine();
-            string reversedString = "";
-            for (int i2 = reverse.Length - 1; i2 >= 0; i2--)
-            {
-                reversedString += reverse[i2];
-            }
-            Console.WriteLine(reversedString);
-
-
-
-            Console.ReadLine();
         }
     }
 }

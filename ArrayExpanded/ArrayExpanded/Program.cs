8fcb72d124c1cafaabdb778a82906ca9352145b1
Kaspern95 <34809965+Kaspern95@users.noreply.github.com>
2018-10-09 09:34:59 +0200

    structs

---
+++ ArrayExpanded/ArrayExpanded/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExpanded
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            //Opg7_9();
+            //Opg7_10();
+            //Opg7_11();
+            //Opg7_12();
+
+            
+            Console.ReadLine();
+        }
+
+        private static void Opg7_12()
+        {
+            int[,] mit2DArray = new int[2, 2];
+            mit2DArray[0, 0] = 7;
+            mit2DArray[1, 0] = 5;
+            mit2DArray[0, 1] = 12;
+            mit2DArray[1, 1] = 34;
+
+            Console.WriteLine("Indtast et nummer i 2D array");
+            string read = Console.ReadLine();
+            int search = int.Parse(read);
+            Contains(mit2DArray, search);
+        }
+
+        private static int[,] Contains(int[,] newArray, int search)
+        {
+            bool exists = false;
+            for (int i = 0; i < newArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < newArray.GetLength(1); j++)
+                {
+                if (newArray[i, j] == search)
+                {
+                    exists = true;
+                        Console.WriteLine($"{search} number does exist in the array");
+                }
+                    
+
+                }
+            }
+            if (exists == false)
+            {
+                Console.WriteLine($"{search} does not exist");
+            }
+            return newArray;
+        }
+
+        private static void Opg7_11()
+        {
+            int[] array = new int[100];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(101);
+            }
+            int[] newArray = ArrayChange(array);
+        }
+
+        private static int[] ArrayChange(int[] intArray)
+        {
+            Array.Resize(ref intArray, 105);
+            return intArray;
+        }
+
+        private static void Opg7_10()
+        {
+            int[] array1 = new int[2] { 5, 10 };
+            int[] array2 = new int[3] { 5, 10, 15 };
+            int resultat = Add(array1, array2);
+        }
+
+        private static int Add(int[] arrayX, int[] arrayY)
+        {
+            int arrayXLength = arrayX.Length;
+            int arrayYLength = arrayY.Length;
+            int x = 0;
+            int y = 0;
+
+            for (int i = 0; i < arrayXLength; i++)
+            {
+                x += arrayX[i];
+            }
+            for (int i = 0; i < arrayYLength; i++)
+            {
+                y += arrayY[i];
+            }
+            int resultat = x + y;
+            return resultat;
+        }
+
+        private static void Opg7_9()
+        {
+            Console.WriteLine("Opgave 7.9");
+            for (int i = 0; i < 10; i++)
+            {
+
+                int tal = 1;
+                for (; tal < 11; tal++)
+                {
+                    Console.WriteLine(tal);
+                }
+                for (tal = 10; tal > 0; tal--)
+                {
+                    Console.WriteLine(tal);
+                }
+                Console.WriteLine($"Omgang nr: {i + 1}");
+            }
+        }
+    }
+}
---
+++ StructsIntro/StructsIntro/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsIntro
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+            int tal = 5;
+            ChangeNumber(ref tal);
+            string tekst = "hej";
+            ChangeTekst(ref tekst);
+
+            Bil skoda = new Bil("V8", 4, true, "NyBil204", 5, false);
+            Mechanic jens = new Mechanic();
+            jens.nyPlade(ref skoda, "Ny plade til skoda");
+
+            skoda.RemoveDoor();
+
+            Console.WriteLine(skoda.antalDore);
+            Bil ferarri = new Bil();
+            
+
+            Console.WriteLine(skoda.motorType);
+
+            Calculator calc = new Calculator();
+            calc.Add();
+
+            Console.WriteLine(tal);
+             */
+
+            Person p1 = new Person("Jens", 25, 1.80);
+
+            //defining with enum
+            //b1.size = BeerBox.BottleSize.Large;
+            BeerBox b1 = new BeerBox(BeerBox.BottleSize.Large, "Hancock", "Hvede", 30, 40, 50, "Denmark", 4, 15.6);
+            Student s1 = new Student();
+            s1.name = "Martin";
+            s1.age = 27;
+            s1.education = "Datamatiker";
+            s1.money = 500;
+            s1.BuyBeer(b1);
+            s1.DrinkBeer();
+           
+
+            Console.WriteLine(s1.myBeer.amount);
+            
+
+            Console.ReadLine();
+        }
+
+        private static void ChangeTekst(ref string tekst)
+        {
+            tekst = "ny tekst";
+        }
+
+        private static void ChangeNumber(ref int tal)
+        {
+            tal = 10;
+        }
+    }
+    struct Student
+    {
+        public string name;
+        public string education;
+        public int age;
+        public double money;
+        public BeerBox myBeer;
+        public void BuyBeer(BeerBox beerToBuy)
+        {
+            if (age > 17 && money >= beerToBuy.salePrice)
+            {
+                myBeer = beerToBuy;
+                money -= beerToBuy.salePrice;
+            }
+        }
+        public void DrinkBeer()
+        {
+            if (myBeer.amount > 0)
+            {
+                myBeer.amount--;
+            }
+        }
+    }
+    struct BeerBox
+    {
+        public enum BottleSize
+        {
+            Small = 25,
+            Medium = 33,
+            Large = 50
+        }
+        public string name;
+        public string hopsType;
+        public int amount;
+        public double price;
+        public double salePrice;
+        public string country;
+        public int rating;
+        public double pct;
+        public BottleSize size;
+        public BeerBox(BottleSize size, string name, string hopsType, int amount,
+        double price, double salePrice, string country, int rating, double pct)
+        {
+            this.size = size;
+            this.name = name;
+            this.hopsType = hopsType;
+            this.amount = amount;
+            this.price = price;
+            this.salePrice = salePrice;
+            this.country = country;
+            this.rating = rating;
+            this.pct = pct;
+        }
+        
+    }
+    struct Person
+    {
+        public string name;
+        public int age;
+        public double height;
+
+        public Person(string name, int age, double height)
+        {
+            this.name = name;
+            this.age = age;
+            this.height = height;
+        }
+    }
+    struct Bil
+    {
+        public Bil(string motorType, int antalHjul, bool _duftFrisker, string _nrPladeNr, int _antalDore, bool _runningMotor)
+        {
+            this.motorType = motorType;
+            this.antalHjul = antalHjul;
+            duftFrisker = _duftFrisker;
+            nrPladeNr = _nrPladeNr;
+            antalDore = _antalDore;
+            runningMotor = _runningMotor;
+        }
+        public string motorType;
+        public int antalHjul;
+        public bool duftFrisker;
+        public string nrPladeNr;
+        public int antalDore;
+        public bool runningMotor;
+
+        public void RemoveDoor()
+        {
+            antalDore--;
+        }
+    }
+    struct Mechanic
+    {
+        public void nyPlade(ref Bil bil, string nytNr)
+        {
+            bil.nrPladeNr = nytNr;
+        }
+
+    }
+    struct Calculator
+    {
+        public void Add()
+        {
+            
+        }
+        public void Sub()
+        {
+
+        }
+    }
+}

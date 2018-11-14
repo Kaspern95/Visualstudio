052565f1d4cbda71a4b80d51644ec83dcd8e8f4f
Kaspern95 <34809965+Kaspern95@users.noreply.github.com>
2018-11-14 21:55:04 +0100

    1

--- WPFTry/WPFTry/MainWindow.xaml.cs
+++ WPFTry/WPFTry/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace WPFTry
 {
@@ -19,72 +20,145 @@ namespace WPFTry
         {
             string name = "rock";
 
+            BitmapImage playerimg = new BitmapImage();
+            playerimg.BeginInit();
+            playerimg.UriSource = new Uri(@"C:\Users\kasper\Documents\GitHub\Visualstudio\WPFTry\WPFTry\bin\Debug\img\rock.png");
+            playerimg.EndInit();
+            playerImage.Source = playerimg;
+
             int number = InstantiatingRPS(name, 0);
             int aiNumber = CreateNumber();
+            string url = CreateImg(aiNumber);
+
+            BitmapImage aiimg = new BitmapImage();
+            aiimg.BeginInit();
 
             if (aiNumber == number)
             {
                 Output.Content = "Tied!";
+                aiimg.UriSource = new Uri(url);
             }
             else if (aiNumber == 2)
             {
                 Output.Content = "You Lost!";
+                aiimg.UriSource = new Uri(url);
             }
             else if (aiNumber == 3)
             {
                 Output.Content = "You Win!";
+                aiimg.UriSource = new Uri(url);
             }
+            aiimg.EndInit();
+            aiImage.Source = aiimg;
+
             DisableButtons();
         }
+
         private void Paper_Click(object sender, RoutedEventArgs e)
         {
             string name = "paper";
-            
+
+            BitmapImage playerimg = new BitmapImage();
+            playerimg.BeginInit();
+            playerimg.UriSource = new Uri(@"C:\Users\kasper\Documents\GitHub\Visualstudio\WPFTry\WPFTry\bin\Debug\img\paper.png");
+            playerimg.EndInit();
+            playerImage.Source = playerimg;
+
             int number = InstantiatingRPS(name, 0);
             int aiNumber = CreateNumber();
+            string url = CreateImg(aiNumber);
+
+            BitmapImage aiimg = new BitmapImage();
+            aiimg.BeginInit();
 
             if (aiNumber == number)
             {
                 Output.Content = "Tied!";
+                aiimg.UriSource = new Uri(url);
             }
             else if (aiNumber == 1)
             {
                 Output.Content = "You Win!";
+                aiimg.UriSource = new Uri(url);
             }
             else if (aiNumber == 3)
             {
                 Output.Content = "You Lost!";
+                aiimg.UriSource = new Uri(url);
             }
+            aiimg.EndInit();
+            aiImage.Source = aiimg;
+
             DisableButtons();
         }
 
         private void Scissor_Click(object sender, RoutedEventArgs e)
         {
             string name = "scissor";
-            
+
+            BitmapImage playerimg = new BitmapImage();
+            playerimg.BeginInit();
+            playerimg.UriSource = new Uri(@"C:\Users\kasper\Documents\GitHub\Visualstudio\WPFTry\WPFTry\bin\Debug\img\scissor.png");
+            playerimg.EndInit();
+            playerImage.Source = playerimg;
+
+
             int number = InstantiatingRPS(name, 0);
             int aiNumber = CreateNumber();
+            string url = CreateImg(aiNumber);
+
+            BitmapImage aiimg = new BitmapImage();
+            aiimg.BeginInit();
 
             if (aiNumber == number)
             {
                 Output.Content = "Tied!";
+                aiimg.UriSource = new Uri(url);
+                
             }
             else if (aiNumber == 1)
             {
                 Output.Content = "You Lost!";
+                aiimg.UriSource = new Uri(url);
+                
+            }
+            else if (aiNumber == 2)
+            {
+                Output.Content = "You Win!";
+                aiimg.UriSource = new Uri(url);
+                
+            }
+            aiimg.EndInit();
+            aiImage.Source = aiimg;
+
+            DisableButtons();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Paper.IsEnabled = true;
+            Rock.IsEnabled = true;
+            Scissor.IsEnabled = true;
+            Output.Content = "Result";
+
+            BlankImages();
+        }
+        private string CreateImg(int aiNumber)
+        {
+            string url = "";
+            if (aiNumber == 1)
+            {
+                url = @"C:\Users\kasper\Documents\GitHub\Visualstudio\WPFTry\WPFTry\bin\Debug\img\rock.png";
+            }
+            else if (aiNumber == 2)
+            {
+                url = @"C:\Users\kasper\Documents\GitHub\Visualstudio\WPFTry\WPFTry\bin\Debug\img\paper.png";
             }
             else if (aiNumber == 3)
             {
-                Output.Content = "You Win!";
+                url = @"C:\Users\kasper\Documents\GitHub\Visualstudio\WPFTry\WPFTry\bin\Debug\img\scissor.png";
             }
-            DisableButtons();
+            return url;
         }
-
-        private void DisplayAINumber()
-        {
-            
-        }
-
         private int InstantiatingRPS(string name, int number)
         {
             if (name == "rock")
@@ -116,6 +190,20 @@ namespace WPFTry
             int PCNR = rnd.Next(1, 4);
             AI ai = new AI(PCNR);
             return ai.number;
+        }
+        private void BlankImages()
+        {
+            BitmapImage playerimg = new BitmapImage();
+            playerimg.BeginInit();
+            playerimg.UriSource = new Uri(@"C:\Users\kasper\Documents\GitHub\Visualstudio\WPFTry\WPFTry\bin\Debug\img\blank.png");
+            playerimg.EndInit();
+            playerImage.Source = playerimg;
+
+            BitmapImage aiimg = new BitmapImage();
+            aiimg.BeginInit();
+            aiimg.UriSource = new Uri(@"C:\Users\kasper\Documents\GitHub\Visualstudio\WPFTry\WPFTry\bin\Debug\img\blank.png");
+            aiimg.EndInit();
+            aiImage.Source = aiimg;
         }
     }
 }

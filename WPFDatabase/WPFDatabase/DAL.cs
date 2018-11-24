d2b9bf2362e4acb8ab2fd728b616d2da0eab27c0
Kaspern95 <34809965+Kaspern95@users.noreply.github.com>
2018-11-24 17:02:16 +0100

    database

---
+++ WPFDatabase/WPFDatabase/DAL.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WPFDatabase
+{
+    class DAL
+    {
+        public static void CreatePerson(string name, int age)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=PersonDB; Integrated Security=true;");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            AddParam(cmd, name, "Name", SqlDbType.NVarChar);
+            AddParam(cmd, age, "Age", SqlDbType.Int);
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+            cmd.CommandText = $"INSERT INTO Person VALUES (@Name, @Age)";
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+
+        }
+        public static List<Person> AllPeople()
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=PersonDB; Integrated Security=true;");
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM Person";
+            con.Open();
+            reader = cmd.ExecuteReader();
+            List<Person> allPeople = new List<Person>();
+
+            while (reader.Read())
+            {
+                allPeople.Add(new Person(reader[1].ToString(), (int)reader[2]));
+            }
+            con.Close();
+
+            return allPeople;
+        }
+        public static void UpdatePerson(string name, int age, string newName, int newAge)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=PersonDB; Integrated Security=true");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"UPDATE PERSON SET name = '{newName}', age = {newAge} WHERE name = '{name}' AND age = {age}";
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+        public static void UpdatePerson(string name, string newName, int newAge)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=PersonDB; Integrated Security=true");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"UPDATE PERSON SET name = '{newName}', age = {newAge} WHERE name = '{name}'";
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public static void UpdatePerson(int age, string newName, int newAge)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=PersonDB; Integrated Security=true");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"UPDATE PERSON SET name = '{newName}', age = {newAge} WHERE age = {age}";
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public static void DeletePerson(string name, int age)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=PersonDB;Integrated Security=true;");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            AddParam(cmd, name, "Name", SqlDbType.NVarChar);
+            AddParam(cmd, age, "Age", SqlDbType.Int);
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "DELETE FROM Person WHERE name = @Name AND age = @Age";
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public static List<Person> SelectPerson(string name, int age)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=PersonDB;Integrated Security=true;");
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+            cmd.Connection = con;
+            AddParam(cmd, name, "Name", SqlDbType.NVarChar);
+            AddParam(cmd, age, "Age", SqlDbType.Int);
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM Person";
+            con.Open();
+            reader = cmd.ExecuteReader();
+
+            List<Person> selectPerson = new List<Person>();
+            while (reader.Read())
+            {
+                if (reader[1].ToString() == name && (int)reader[2] == age)
+                {
+                    selectPerson.Add(new Person(reader[1].ToString(), (int)reader[2]));
+                }
+            }
+
+            con.Close();
+            return selectPerson;
+        }
+        private static void AddParam(SqlCommand cmd, object value, string name, SqlDbType sqlDbType)
+        {
+            SqlParameter parameter = new SqlParameter();
+            parameter.ParameterName = "@" + name;
+            parameter.Value = value;
+            parameter.SqlDbType = sqlDbType;
+            parameter.Size = 255;
+            cmd.Parameters.Add(parameter);
+        }
+    }
+}
---
+++ WPFDatabase/WPFDatabase/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFDatabase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            string name = TextName.Text;
+            int age = int.Parse(TextAge.Text);
+            DAL.CreatePerson(name, age);
+        }
+
+        private void Read_Click(object sender, RoutedEventArgs e)
+        {
+            List<Person> people = DAL.AllPeople();
+
+            ListBox.ItemsSource = people;
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            List<Person> people = DAL.AllPeople();
+            int value;
+            string name = TextUpdateName.Text;
+            string newName = TextName.Text;
+            int newAge = int.Parse(TextAge.Text);
+            for (int i = 0; i < people.Count; i++)
+            {
+
+                if ((int.TryParse(TextUpdateAge.Text, out value) && people[i].Age == value && people[i].Name == name))
+                {
+                    DAL.UpdatePerson(name, value, newName, newAge);
+                }
+                else if (people[i]. Age == value)
+                {
+                    DAL.UpdatePerson(value, newName, newAge);
+                }
+                else if (people[i].Name == name)
+                {
+                    DAL.UpdatePerson(name, newName, newAge);
+                }
+            }
+            
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            string name = TextName.Text;
+            int age = int.Parse(TextAge.Text);
+            DAL.DeletePerson(name, age);
+        }
+
+        private void Select_Click(object sender, RoutedEventArgs e)
+        {
+            string name = TextName.Text;
+            int age = int.Parse(TextAge.Text);
+            List<Person> selectPerson = DAL.SelectPerson(name, age);
+            ListBox.ItemsSource = selectPerson;
+        }
+    }
+}

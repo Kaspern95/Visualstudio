1b1d5cdd4945a3bfd13e194d1c6d32a1e11e24a7
Kaspern95 <34809965+Kaspern95@users.noreply.github.com>
2018-11-28 09:24:04 +0100

    databinding

---
+++ DataBindingFollowup/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DataBindingFollowup
+{
+    class Person : INotifyPropertyChanged
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}

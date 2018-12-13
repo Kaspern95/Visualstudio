7b77ba345b295b2e20dc5f217fd56f677f48a897
Kaspern95 <34809965+Kaspern95@users.noreply.github.com>
2018-12-13 13:29:07 +0100

    1

---
+++ WPFDatabaseExceptionHandling/WPFDatabase/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDatabase
+{
+    class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return this.Name + ", " + Age;
+        }
+    }
+}
